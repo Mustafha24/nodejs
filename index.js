c29f3515560cad52a6d94f7494ede548e2029f4b
Ahmad <112511041+Mustafha24@users.noreply.github.com>
2023-06-11 10:34:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 const http = require("https");
 const fs = require("fs");
-const express=require('express')
-const app=express()
-const getdata = () => {
-  return new Promise((resolve, reject) => {
-    http.get("https://reqres.in/api/users", (res) => {
-      let data = "";
-      res.on("data", (chunk) => {
-        data += chunk;
-      });
-      res.on("end", () => {
-        data = JSON.parse(data);
-        resolve(data);
+const express = require("express");
+const { log } = require("console");
+const app = express();
+
+app.get("/", (req, res) => {
+  const getdata = () => {
+    return new Promise((resolve, reject) => {
+      http.get("https://reqres.in/api/users", (res) => {
+        let data = "";
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+        res.on("end", () => {
+          data = JSON.parse(data);
+          resolve(data);
+        });
       });
     });
-    
-  });
-};
-app.get('/',(req,res)=>{
+  };
 
-async function name() {
-  console.log("BEFORE FETCH");
-  const final = await getdata();
-  res.send(final)
-  console.log(final);
-  console.log("AFTER FETCH");
+  async function name() {
+    console.log("BEFORE FETCH");
+    const final = await getdata();
+    console.log(final);
+    res.send(final);
+    console.log("AFTER FETCH");
+  }
   name();
-}
-})
+});
 
+app.listen(5000,()=>{
+    console.log('server started');
+})
