58e4ca10f23b5d1c720adf40320e5538587b4ea1
Ahmad <112511041+Mustafha24@users.noreply.github.com>
2023-06-11 10:23:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require("https");
 const fs = require("fs");
+const express=require('express')
+const app=express()
 const getdata = () => {
   return new Promise((resolve, reject) => {
     http.get("https://reqres.in/api/users", (res) => {
@@ -15,10 +17,14 @@ const getdata = () => {
     
   });
 };
+app.get('/',(req,res)=>{
+
 async function name() {
   console.log("BEFORE FETCH");
   const final = await getdata();
+  res.send(final)
   console.log(final);
   console.log("AFTER FETCH");
 }
+})
 name();
